205eba624cb7318dffe8404d14d98e0fcf7b2d85
Andrey Fatahov <fotike.pr@gmail.com>
2024-09-02 02:50:34 +0300

    Created testimonials section

---
+++ src/js/components/home/slider.js
@@ -0,0 +1,42 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import { EffectFade } from 'swiper/modules';
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    modules: [Navigation],
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    navigation: {
+      nextEl: '.insight__slider-next',
+      prevEl: '.insight__slider-prev',
+    },
+    centeredSlides: true,
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
+
+export const useTestimonialsSlider = () => {
+  new Swiper('.testimonials__slider', {
+    modules: [Navigation, EffectFade],
+    container: '.testimonials__slider',
+    effect: 'fade',
+    slidesPerView: 1,
+    spaceBetween: 180,
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.testimonials__btn--next',
+      prevEl: '.testimonials__btn--prev',
+    },
+    fadeEffect: {
+      crossFade: true,
+    },
+  });
+};
