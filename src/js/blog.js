23c174b15ea7cb87ef620533ec80bd9e6966279a
Andrey Fatahov <fotike.pr@gmail.com>
2024-09-12 22:00:39 +0300

    Created blog page

---
+++ src/js/blog.js
@@ -0,0 +1,10 @@
+import '/scss/blog.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useAccordion } from './components/accordion.js';
+
+useTheme();
+useBurger();
+useAccordion();
